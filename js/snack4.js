5870a4e51c7c67056d359f52d30a12bfe166553d
GLCVLL <g.ioele96@outlook.it>
2023-06-14 19:08:52 +0200

    added teams object array

--- js/snack4.js
+++ js/snack4.js
@@ -5,3 +5,13 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// creo array di oggetti squadre di calcio
+const teams = [
+    { name: 'Roma', points: getRandomNumber(0, 100), foulsSoffered: getRandomNumber(0, 40) },
+    { name: 'Inter', points: getRandomNumber(0, 100), foulsSoffered: getRandomNumber(0, 40) },
+    { name: 'Milan', points: getRandomNumber(0, 100), foulsSoffered: getRandomNumber(0, 40) },
+    { name: 'Juve', points: getRandomNumber(0, 100), foulsSoffered: getRandomNumber(0, 40) },
+    { name: 'Napoli', points: getRandomNumber(0, 100), foulsSoffered: getRandomNumber(0, 40) },
+]
+
+console.log(teams);
