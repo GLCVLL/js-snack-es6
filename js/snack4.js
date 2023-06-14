5ea77e6abde2c8212b0cf7b2fe3e4a654b4acf13
GLCVLL <g.ioele96@outlook.it>
2023-06-14 19:07:50 +0200

    added random numbers function

--- js/snack4.js
+++ js/snack4.js
@@ -1,2 +1,7 @@
 console.log('js ok');
 
+// Funzione per generare un numero casuale compreso tra min e max
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
