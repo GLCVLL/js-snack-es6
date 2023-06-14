bd1fdcc8d263f6cecaea3fd88c383f5a9801a00c
GLCVLL <g.ioele96@outlook.it>
2023-06-14 15:49:57 +0200

    added object array

--- js/snack3.js
+++ js/snack3.js
@@ -1,2 +1,12 @@
 console.log('JS OK');
 
+
+// Creazione dell'array di oggetti
+const bikes = [
+    { name: 'Bici 1', weight: 8 },
+    { name: 'Bici 2', weight: 7 },
+    { name: 'Bici 3', weight: 6 },
+    { name: 'Bici 4', weight: 9 },
+    { name: 'Bici 5', weight: 10},
+];
+
